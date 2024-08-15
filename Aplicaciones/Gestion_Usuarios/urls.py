1b7b0a254371e632ee9494c4e9117c83a86c4626
Georky <georky27condoy@gmail.com>
2024-08-15 14:31:39 -0500

    quinto commit

--- Aplicaciones/Gestion_Usuarios/urls.py
+++ Aplicaciones/Gestion_Usuarios/urls.py
@@ -9,4 +9,4 @@ urlpatterns = [
     path('registrarUsuarios/', views.registrarUsuarios,name='registrarUsuarios'),
     path('eliminarUsuarios/<str:telefono>/', views.eliminarUsuarios, name='eliminarUsuarios'),
     path('enviarNotifi/<str:telefono>/<str:nombreC>/<str:mensaje>/', views.enviarNotifi,name='enviarNotifi'),
-    ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    ]+ static(settings.STATIC_URL, document_root=settings.MEDIA_ROOT)
