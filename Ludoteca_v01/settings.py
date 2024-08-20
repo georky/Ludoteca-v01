85263671d087cc59fa8869cbdbc0063a2d2196e9
Georky <georky27condoy@gmail.com>
2024-08-19 21:28:19 -0500

    capturar errores1

--- Ludoteca_v01/settings.py
+++ Ludoteca_v01/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q+uk%otgznyl0eifxl_=$z3)__mnwjue(pw7#gi+zy%a&v&q0-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
